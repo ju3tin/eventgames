919acbecfecc72c9ff62fbb2f585268f04f82b2b
Justin <ju3tin@hotmail.co.uk>
2026-01-31 22:27:01 +0000

    sd

--- app/pages/hello1.ts
+++ app/pages/hello1.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
  
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
+  if (req.method === 'GET') {
     // Process a POST request
   } else {
     // Handle any other HTTP method
