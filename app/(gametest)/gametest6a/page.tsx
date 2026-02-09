64a18306cb3da8f23f94da5a1fdca52a2d1f8e6e
Justin <ju3tin@hotmail.co.uk>
2026-02-09 13:45:57 +0000

    mnmn

--- app/(gametest)/gametest6a/page.tsx
+++ app/(gametest)/gametest6a/page.tsx
@@ -414,8 +414,8 @@ export default function AirJugglerPage() {
           className="overlay"
           style={{
             position: 'absolute',
-            top: 0,
-            left: 0,
+            top: '50%',
+            left: '50%',
             width: 640,
             height: 480,
             background: 'rgba(0,0,0,0.6)',
