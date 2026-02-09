cad0f2c7f7ae6c52d4a677ba788055bba6f6d2b8
Justin <ju3tin@hotmail.co.uk>
2026-02-09 22:40:23 +0000

    Update page.tsx

--- app/(games)/games/page.tsx
+++ app/(games)/games/page.tsx
@@ -91,7 +91,6 @@ export default async function GamesPage() {
                 color={game.color}
                 link={game.link}
                 isLocked={game.isLocked}
-                slug={game.slug}
               />
             ))}
           </div>
