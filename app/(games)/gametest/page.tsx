ec40f61e54a503fc3a89645baa47aecd46202432
Justin <ju3tin@hotmail.co.uk>
2026-02-09 10:11:33 +0000

    dfgdg

--- app/(games)/gametest/page.tsx
+++ app/(games)/gametest/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react"
 import { createClient } from '@/lib/supabase/client'
 import { useRouter } from 'next/navigation'
 import { User } from '@supabase/supabase-js'
+import styles from '@app/public/styles/gametest.module.css'
 
 type GameOption = {
   game_id: string  // uuid as string
