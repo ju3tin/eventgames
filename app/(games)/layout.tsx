7336e77bd0cfbfd35d719788d9ab74f54d6bbd5c
Justin <ju3tin@hotmail.co.uk>
2026-02-11 11:06:27 +0000

    sdf

--- app/(games)/layout.tsx
+++ app/(games)/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
       <body className={`font-sans antialiased`}>
         <SidebarLayout user={user}>
         {children}
-          <SidebarLayout />
+          </ SidebarLayout>
         <Analytics />
       </body>
     </html>
