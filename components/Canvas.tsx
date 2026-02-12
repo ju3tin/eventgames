b9e4906d18bdebd8b835b3b46bc38c72f31bc675
Justin <ju3tin@hotmail.co.uk>
2026-02-12 11:21:04 +0000

    Update Canvas.tsx

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -1,6 +1,6 @@
 // /components/Canvas.tsx
 "use client"
-import { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 
 interface CanvasProps {
   canvasWidth: number;
@@ -13,7 +13,8 @@ interface CanvasProps {
   onChange: (canvas: any) => void;
 }
 
-const Canvas = ({
+// Forward ref to the canvas element
+const Canvas = React.forwardRef<HTMLCanvasElement, CanvasProps>(({
   canvasWidth,
   canvasHeight,
   brushColor,
@@ -22,39 +23,50 @@ const Canvas = ({
   gridSize,
   snapToGrid,
   onChange,
-}: CanvasProps) => {
-  const canvasRef = useRef<any>(null);
+}, ref) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  // When the component mounts, we use the forwarded ref to point to the canvas element
+  useEffect(() => {
+    if (ref) {
+      // @ts-ignore Ignore TypeScript error for ref forwarding
+      ref.current = canvasRef.current;
+    }
+  }, [ref]);
 
   const handleDrawing = () => {
     const canvas = canvasRef.current;
-    const ctx = canvas.drawing.getContext('2d');
+    if (!canvas) return;
+    
+    const ctx = canvas.getContext('2d');
     const gridSizePixels = gridSize;
-    const rect = canvas.drawing.getBoundingClientRect();
+    const rect = canvas.getBoundingClientRect();
     const drawX = Math.floor((rect.left - canvas.offsetLeft) / gridSizePixels) * gridSizePixels;
     const drawY = Math.floor((rect.top - canvas.offsetTop) / gridSizePixels) * gridSizePixels;
 
-    ctx.beginPath();
+    ctx?.beginPath();
     if (brushShape === 'circle') {
-      ctx.arc(drawX, drawY, brushSize, 0, Math.PI * 2);
+      ctx?.arc(drawX, drawY, brushSize, 0, Math.PI * 2);
     } else {
-      ctx.rect(drawX - brushSize / 2, drawY - brushSize / 2, brushSize, brushSize);
+      ctx?.rect(drawX - brushSize / 2, drawY - brushSize / 2, brushSize, brushSize);
     }
-    ctx.fillStyle = brushColor;
-    ctx.fill();
+    ctx!.fillStyle = brushColor;
+    ctx?.fill();
     onChange(canvas);  // Notify parent about the canvas change
   };
 
   return (
-    <div>
+    <div className="canvas-container">
       <canvas
         ref={canvasRef}
         width={canvasWidth}
         height={canvasHeight}
         onMouseMove={handleDrawing}
         onMouseDown={handleDrawing}
+        className="border"
       />
     </div>
   );
-};
+});
 
 export default Canvas;
