1c17fad125c815342c72b40d9cc87ac74985f243
ju3tin <ju3tin@hotmail.co.uk>
2026-02-17 21:42:20 +0000

    sdf

--- components/uis/toaster.tsx
+++ components/uis/toaster.tsx
@@ -8,7 +8,7 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from '@/components/ui/toast'
+} from '@/components/uis/toast'
 
 export function Toaster() {
   const { toasts } = useToast()
