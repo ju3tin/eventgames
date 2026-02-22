4e10602dbe3d0b2985245ede96e92a46269e13fd
Justin <ju3tin@hotmail.co.uk>
2026-02-22 15:35:34 +0000

    Create menu.js

---
+++ public/js/menu.js
@@ -0,0 +1,40 @@
+$(document).on('pagecreate', '#menuPage', function() {
+    $.getJSON('menu.json', function(data) {
+        const $menu = $('#dynamicMenu');
+        
+        function buildMenu(items, $parent) {
+            items.forEach(item => {
+                // If it has a "title", create a nested collapsible set
+                if (item.title) {
+                    const $li = $('<li></li>');
+                    const $collapsible = $(`
+                        <div data-role="collapsible">
+                            <h3>${item.title}</h3>
+                            <ul data-role="listview"></ul>
+                        </div>
+                    `);
+                    buildMenu(item.items, $collapsible.find('ul'));
+                    $li.append($collapsible);
+                    $parent.append($li);
+                }
+                // Leaf items
+                else {
+                    const $li = $('<li></li>');
+                    if (item.icon) $li.attr('data-icon', item.icon);
+                    if (item.filterText) $li.attr('data-filtertext', item.filterText);
+                    if (item.class) $li.addClass(item.class);
+
+                    const $a = $('<a></a>').attr('href', item.href);
+                    if (item.ajax === false) $a.attr('data-ajax', 'false');
+                    $a.text(item.text);
+
+                    $li.append($a);
+                    $parent.append($li);
+                }
+            });
+        }
+
+        buildMenu(data.menu, $menu);
+        $menu.listview().listview('refresh');
+    });
+});
