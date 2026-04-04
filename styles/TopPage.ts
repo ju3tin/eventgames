1f7d05eb4f1cdfc7777d753bc662893366ecfa1b
Justin <ju3tin@hotmail.co.uk>
2026-04-04 01:41:46 +0100

    Add files via upload

---
+++ styles/TopPage.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const SmallText = styled.p`
+  font-family: 'Noto Sans JP', sans-serif;
+  font-weight: 700;
+  font-size: 20px;
+  color: #0a2569;
+  margin: 0;
+`
+
+export const ReturnButton = styled.img`
+  cursor: pointer;
+  position: absolute;
+  right: 10px;
+  top: 10px;
+}`
+
+export const Buttons = styled.div`
+  position: fixed;
+  margin: auto;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  gap: 5px;
+  justify-content: center;
+`
