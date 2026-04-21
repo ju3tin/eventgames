57745fe3097481895355e847837e60ae395fc604
Justin <ju3tin@hotmail.co.uk>
2026-04-22 00:16:46 +0100

    Update page.tsx

--- app/(events)/eventpage/page.tsx
+++ app/(events)/eventpage/page.tsx
@@ -1,10 +1,24 @@
 // pages/page.tsx
 "use client"
+import Head from 'next/head';
 import WebcamCanvas from '@/components/WebcamCanvas';
 
 const Page = () => {
   return (
     <div>
+       <Head>
+        {/* Load TensorFlow.js from CDN */}
+        <script
+          src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.0.0/dist/tf.min.js"
+          type="text/javascript"
+        />
+        {/* Load MoveNet model from TensorFlow.js */}
+        <script
+          src="https://cdn.jsdelivr.net/npm/@tensorflow-models/pose-detection@2.0.0/dist/pose-detection.min.js"
+          type="text/javascript"
+        />
+      </Head>
+      
       {/* <h1 style={{ textAlign: 'center', marginTop: '20px' }}>Webcam Canvas</h1> */}
       <WebcamCanvas />
     </div>
