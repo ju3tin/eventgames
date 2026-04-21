9935f228172163f0ba9c2152523e2c30230cb1fc
Justin <ju3tin@hotmail.co.uk>
2026-04-22 00:45:35 +0100

    Update page.tsx

--- app/(events)/eventpage/page.tsx
+++ app/(events)/eventpage/page.tsx
@@ -1,28 +1,11 @@
-// pages/page.tsx
 "use client"
-import Head from 'next/head';
-import WebcamCanvas from '@/components/WebcamCanvas';
+import dynamic from 'next/dynamic';
 
-const Page = () => {
-  return (
-    <div>
-       <Head>
-        {/* Load TensorFlow.js from CDN */}
-        <script
-          src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.0.0/dist/tf.min.js"
-          type="text/javascript"
-        />
-        {/* Load MoveNet model from TensorFlow.js */}
-        <script
-          src="https://cdn.jsdelivr.net/npm/@tensorflow-models/pose-detection@2.0.0/dist/pose-detection.min.js"
-          type="text/javascript"
-        />
-      </Head>
-      
-      {/* <h1 style={{ textAlign: 'center', marginTop: '20px' }}>Webcam Canvas</h1> */}
-      <WebcamCanvas />
-    </div>
-  );
-};
+const WebcamCanvas = dynamic(
+  () => import('../components/WebcamCanvas'),
+  { ssr: false }
+);
 
-export default Page;
+export default function Home() {
+  return <WebcamCanvas />;
+}
