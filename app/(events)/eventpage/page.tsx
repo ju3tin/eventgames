2c91aada4e8b343f4590093ee8195a83ed9b7ce5
Justin <ju3tin@hotmail.co.uk>
2026-04-22 00:47:46 +0100

    Update page.tsx

--- app/(events)/eventpage/page.tsx
+++ app/(events)/eventpage/page.tsx
@@ -2,7 +2,7 @@
 import dynamic from 'next/dynamic';
 
 const WebcamCanvas = dynamic(
-  () => import('../components/WebcamCanvas'),
+  () => import('@/components/WebcamCanvas'),
   { ssr: false }
 );
 
