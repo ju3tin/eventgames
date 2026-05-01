990ad0ff05ec66625fb0820f55073dd38dd75f38
Justin <ju3tin@hotmail.co.uk>
2026-05-01 17:37:01 +0100

    Define constants for Solana program and mint
    Added constants for PROGRAM_ID, MINT, and RANDOM_STRING.

---
+++ lib/constants.ts
@@ -0,0 +1,11 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const PROGRAM_ID = new PublicKey(
+  "DNjSQoQ7u9Zrfot4uaATGbszQyF8HhRpbYGBy11eMQ7t"
+);
+
+export const MINT = new PublicKey(
+  "5vQnjwhBHex9rVJ4KbUkMuz3TkfPsYZB1JQFrZn7yJpb"
+);
+
+export const RANDOM_STRING = "abc12345";
