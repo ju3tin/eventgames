6399c4ad4e8e0997db5e50c6fa3ae0f7a0d30941
ju3tin <ju3tin@hotmail.co.uk>
2026-05-05 07:40:51 +0100

    sdf

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,8 @@
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push(
-        '@solana/web3.js',
-        '@coral-xyz/anchor',
-      );
-    }
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      os: false,
-      path: false,
-      crypto: false,
-    };
-    return config;
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
